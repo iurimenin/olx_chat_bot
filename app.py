7bc162a30cb13b63ca480a052fc6675b112386e8
Iuri Menin <iurimenin@gmail.com>
2017-08-04 16:18:09 -0300

    Test

--- app.py
+++ app.py
@@ -32,52 +32,53 @@ def olx_bot():
 
     login.click()
 
-    listAllLinks = []
-    listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/grande-florianopolis/veiculos')
-    listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/veiculos')
-    listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/oeste-de-santa-catarina/regioes-de-curitibanos-e-c-dos-lages/veiculos')
-    listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/norte-de-santa-catarina/veiculos/')
-
-    countSendMessage = 0
-    for link in listAllLinks:
-        json.dumps(link)
-        driver.get(link)
-
-        showChat = driver.find_element_by_class_name('chat-client-wrapper')
-
-        buttonShowChat = showChat.find_element_by_tag_name('button')
-        buttonShowChat.click()
-
-        chatContainer = driver.find_element_by_id('chat_container')
-
-        listMessages = chatContainer.find_element_by_class_name('list-messages')
-
-        time.sleep(5)
-
-        listMessagesItens = listMessages.find_elements_by_tag_name('li')
-        if len(listMessagesItens) == 0:
-
-            message = chatContainer.find_element_by_name('message')
-
-            message.send_keys(
-                'Caro amigo, vi que estás vendendo seu caminhão, onde caso algum cliente se interessar nele, '
-                'e precisar financiar uma parte, estou à disposição, faço financiamento e '
-                'refinanciamento (capital de giro) de caminhões usados à partir do ano de 1970, '
-                'sem restrição de marca e modelo, e com foco em primeiro caminhão. '
-                'Estou à disposição, att Oliandro Omni Financeira 48 999249090 (Tim e Whatsapp)'
-                    .decode('UTF-8'))
-            sendMessage = chatContainer.find_element_by_name('sendMessage')
-
-            sendMessage.click()
-
-            chatContainer.find_element_by_class_name('chat-close').click()
-
-            countSendMessage = countSendMessage + 1
-        else:
-            continue
-
-    returnMessage = 'Foram enviadas ' + str(countSendMessage) + ' mensagens!!'
-    print returnMessage
+    return  "OK"
+    # listAllLinks = []
+    # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/grande-florianopolis/veiculos')
+    # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/veiculos')
+    # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/oeste-de-santa-catarina/regioes-de-curitibanos-e-c-dos-lages/veiculos')
+    # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/norte-de-santa-catarina/veiculos/')
+    #
+    # countSendMessage = 0
+    # for link in listAllLinks:
+    #     json.dumps(link)
+    #     driver.get(link)
+    #
+    #     showChat = driver.find_element_by_class_name('chat-client-wrapper')
+    #
+    #     buttonShowChat = showChat.find_element_by_tag_name('button')
+    #     buttonShowChat.click()
+    #
+    #     chatContainer = driver.find_element_by_id('chat_container')
+    #
+    #     listMessages = chatContainer.find_element_by_class_name('list-messages')
+    #
+    #     time.sleep(5)
+    #
+    #     listMessagesItens = listMessages.find_elements_by_tag_name('li')
+    #     if len(listMessagesItens) == 0:
+    #
+    #         message = chatContainer.find_element_by_name('message')
+    #
+    #         message.send_keys(
+    #             'Caro amigo, vi que estás vendendo seu caminhão, onde caso algum cliente se interessar nele, '
+    #             'e precisar financiar uma parte, estou à disposição, faço financiamento e '
+    #             'refinanciamento (capital de giro) de caminhões usados à partir do ano de 1970, '
+    #             'sem restrição de marca e modelo, e com foco em primeiro caminhão. '
+    #             'Estou à disposição, att Oliandro Omni Financeira 48 999249090 (Tim e Whatsapp)'
+    #                 .decode('UTF-8'))
+    #         sendMessage = chatContainer.find_element_by_name('sendMessage')
+    #
+    #         sendMessage.click()
+    #
+    #         chatContainer.find_element_by_class_name('chat-close').click()
+    #
+    #         countSendMessage = countSendMessage + 1
+    #     else:
+    #         continue
+    #
+    # returnMessage = 'Foram enviadas ' + str(countSendMessage) + ' mensagens!!'
+    # print returnMessage
     # return Response(json.dumps(returnMessage),  mimetype='application/json')
 
 def getLinks(driver, urlBase):
