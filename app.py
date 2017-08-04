4adef092e6d27b8c565a38cbf3ca6c2a52f1297a
Iuri Menin <iurimenin@gmail.com>
2017-08-04 16:30:27 -0300

    Test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import json
 import time
+import os
 from decouple import config
 
 from flask import json, Response
@@ -14,25 +15,25 @@ def olx_bot():
 
     GOOGLE_CHROME_BIN = config('GOOGLE_CHROME_BIN')
     CHROMEDRIVER_PATH = config('CHROMEDRIVER_PATH')
+    return "OK"
+    # chrome_options = Options()
+    # chrome_options.binary_location = GOOGLE_CHROME_BIN
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--no-sandbox')
+    #
+    # driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    # driver.get('https://www3.olx.com.br/account/form_login/')
+    #
+    # email = driver.find_element_by_id('login_email')
+    # password = driver.find_element_by_id('login_password')
+    # login = driver.find_element_by_id('bt_submit_login')
+    #
+    # email.send_keys('criciuma@agenteomni.com.br')
+    # password.send_keys('260102')
+    #
+    # login.click()
 
-    chrome_options = Options()
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
 
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-    driver.get('https://www3.olx.com.br/account/form_login/')
-
-    email = driver.find_element_by_id('login_email')
-    password = driver.find_element_by_id('login_password')
-    login = driver.find_element_by_id('bt_submit_login')
-
-    email.send_keys('criciuma@agenteomni.com.br')
-    password.send_keys('260102')
-
-    login.click()
-
-    return  "OK"
     # listAllLinks = []
     # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/grande-florianopolis/veiculos')
     # listAllLinks = listAllLinks + getLinks(driver, 'http://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/veiculos')
