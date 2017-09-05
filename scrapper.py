6259fb82d012c5c2e472fe777821e1154c19326f
Iuri Menin <iurimenin@gmail.com>
2017-09-05 17:38:45 -0300

    treatment to wait for the advertisement to disappear

--- scrapper.py
+++ scrapper.py
@@ -23,7 +23,14 @@ class Scrapper(threading.Thread):
 
         listAllLinks = []
         listAllLinks = listAllLinks + self.getLinks(driver,
-                                               'http://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/veiculos/caminhoes-onibus-e-vans')
+                                               'http://sc.olx.com.br/florianopolis-e-regiao/'
+                                               'outras-cidades/veiculos/caminhoes-onibus-e-vans'
+                                               '?sd=2609&sd=2597&sd=2567&sd=2579&sd=2613&sd=2616&sd=2610&sd=2569'
+                                               '&sd=2570&sd=2583&sd=2615&sd=2586&sd=2600&sd=2585&sd=2578&sd=2576'
+                                               '&sd=2603&sd=2608&sd=2591&sd=2588&sd=2611&sd=2595&sd=2575&sd=2598'
+                                               '&sd=2617&sd=2571&sd=2580&sd=2568&sd=2601&sd=2599&sd=2593&sd=2582'
+                                               '&sd=2605&sd=2577&sd=2572&sd=2584&sd=2573&sd=2592&sd=2607&sd=2581'
+                                               '&sd=2618&sd=2606&sd=2604&sd=2614&sd=2596')
         listAllLinks = listAllLinks + self.getLinks(driver,
                                                'http://sc.olx.com.br/oeste-de-santa-catarina/regioes-de-curitibanos-e-c-dos-lages/veiculos/caminhoes-onibus-e-vans')
         listAllLinks = listAllLinks + self.getLinks(driver,
@@ -145,7 +152,17 @@ class Scrapper(threading.Thread):
         try:
             gc.collect()
             print('Salvando pesquisa de %s.' % urlBase)
-            driver.get(urlBase + '?q=caminh%C3%A3o')
+
+            if "?" not in urlBase:
+                driver.get(urlBase + '?q=caminh%C3%A3o')
+            else:
+                driver.get(urlBase + '&q=caminh%C3%A3o')
+
+            try:
+                driver.find_elements_by_id('sas-interstitial')
+                time.sleep(15)
+            except:
+                logging.exception("Nenhuma propaganda mostrada")
 
             listAllLinks = self.getUrl(driver)
             pagination = driver.find_element_by_class_name('module_pagination')
